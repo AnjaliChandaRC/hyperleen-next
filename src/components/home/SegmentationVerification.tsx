6204e80c895ddc419fe2c31de14094487e3232ae
rinklepoonia <rinklepoonia889@gmail.com>
2024-07-24 12:12:59 +0530

    deploying

--- src/components/home/SegmentationVerification.tsx
+++ src/components/home/SegmentationVerification.tsx
@@ -1,10 +1,12 @@
+
 import React from 'react'
 
 const SegmentationVerification = () => {
   return (
     <div>
       <h2 className=''>Image Segmentation and Verification</h2>
-      <img src="" alt="" />
+    
+
     </div>
   );
 }
