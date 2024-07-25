14f08f5de5cd03d7c874ceb31f7ec01d2ed0e2ee
rinklepoonia <rinklepoonia889@gmail.com>
2024-07-25 15:29:14 +0530

    change-code

--- src/components/common/CommonButton.tsx
+++ src/components/common/CommonButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 type BUTTON = {
   buttonName?: string;
-  buttonClass: string;
+  buttonClass?: string;
 };
 
 const CommonButton = ({ buttonName, buttonClass }: BUTTON) => {
--- src/components/home/SegmentationVerification.tsx
+++ src/components/home/SegmentationVerification.tsx
@@ -6,8 +6,7 @@ import Image from "next/image";
 const SegmentationVerification = () => {
   return (
     <div className="lg:pt-40 md:pt-20 pt-10">
-      <div className="container px-3 mx-auto">
-        {/* =======segmentation======= */}
+      <div className="container xl:max-w-[1140px] mx-auto">
         <PrimaryHeading
           textName="Image"
           textSpan=" Segmentation and "
@@ -18,7 +17,6 @@ const SegmentationVerification = () => {
           textName="In the realm of sentiment analysis, human judgment plays a crucial role. While our methods may not be extravagant, they are designed to ensure precise and reliable results."
           className="text-center max-w-[740px] mx-auto lg:pb-14 sm:pb-8 pb-5"
         />
-        {/* ======row-col====== */}
         <div className="flex flex-wrap lg:flex-row flex-col-reverse mx-[-12px] items-center">
           <div className="lg:w-[50%] w-full px-3 lg:pt-0 sm:pt-5 pt-3">
             <h5 className="font_gilroy_regular font-semibold lg:text-2xxl text-2xl leading-md text-black lg:pb-4 sm-pb-1 pb-0 max-lg:text-center">
@@ -29,22 +27,23 @@ const SegmentationVerification = () => {
               className="lg:pb-12 sm:pb-5 pb-2 lg:max-w-[540px] max-lg:text-center"
             />
             <div className="max-lg:mx-auto max-lg:text-center">
-              <CommonButton buttonName="Learn More" buttonClass="" />
+              <CommonButton buttonName="Learn More" />
             </div>
           </div>
-          <div className="lg:w-[50%] w-full px-3">
+          <div className="lg:w-[50%] w-full xl:px-3">
             <div className="relative lg:max-w-[502px] sm:max-w-96 max-w-72 max-lg:mx-auto">
               <Image
                 width={502}
                 height={483}
                 src="/assets/images/home/webp/segmentation-img.webp"
                 alt="segmentation-img"
+                className="pointer-events-none"
               />
               <div className="bg-blue max-w-[502px] rounded-t-2xl lg:p-4 p-2 absolute top-0 lg:w-[502px] w-full">
-                <p className="font-poppins font-normal sm:text-lg text-base text-white lg:pb-2 pb-0">
+                <p className="sm:text-lg text-base text-white lg:pb-2 pb-0">
                   Selected All Squares With
                 </p>
-                <p className="font-Gilroy font-semibold lg:text-2xxl sm:text-2xl text-xl lg:leading-md text-white">
+                <p className="font_gilroy_bold lg:text-2xxl sm:text-2xl text-xl lg:leading-md text-white">
                   Car
                 </p>
               </div>
