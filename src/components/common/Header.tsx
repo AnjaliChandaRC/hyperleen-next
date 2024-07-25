1332f5a8c3db65a63ecf19833af4b80f962c74d6
anjali-chanda <anjalichanda2019@gmail.com>
2024-07-25 19:46:55 +0530

    resolve error in header and heri

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -31,19 +31,40 @@ const Header = () => {
   }
 
   return (
-    <nav className='border-b-[0.1px] border-grey py-4'>
-      <div className='max-w-[1140px] mx-auto container flex items-center justify-between w-full'>
-        <div className='w-[54px] h-[56px]'>
-          <Link href='/'>
+    <nav className="border-b-[0.1px] border-grey py-4">
+      <div className="max-w-[1140px] mx-auto container flex items-center justify-between w-full">
+        <div className="w-[54px] h-[56px]">
+          <Link href="/">
             <Header_logo />
           </Link>
         </div>
-        <div className='flex lg:hidden flex-col items-center justify-between w-[30px] h-[22px] z-30 cursor-pointer' onClick={HANDLE_NAVBAR}>
-          <span className={`w-full h-[3px] bg-black ease-in-out duration-300 rounded-[2px] ${!active ? "rotate-45 relative top-[8px] -right-px" : "rotate-0"}`}></span>
-          <span className={`w-full h-[3px] bg-black ease-in-out duration-300 rounded-[2px] ${!active ? "hidden" : "block"}`}></span>
-          <span className={`w-full h-[3px] bg-black ease-in-out duration-300 rounded-[2px] ${!active ? "-rotate-45 relative bottom-[11px] -right-px" : "rotate-0"}`}></span>
+        <div
+          className="flex lg:hidden flex-col items-center justify-between w-[30px] h-[22px] z-30 cursor-pointer"
+          onClick={HANDLE_NAVBAR}
+        >
+          <span
+            className={`w-full h-[3px] bg-black ease-in-out duration-300 rounded-[2px] ${
+              !active ? "rotate-45 relative top-[8px] -right-px" : "rotate-0"
+            }`}
+          ></span>
+          <span
+            className={`w-full h-[3px] bg-black ease-in-out duration-300 rounded-[2px] ${
+              !active ? "hidden" : "block"
+            }`}
+          ></span>
+          <span
+            className={`w-full h-[3px] bg-black ease-in-out duration-300 rounded-[2px] ${
+              !active
+                ? "-rotate-45 relative bottom-[11px] -right-px"
+                : "rotate-0"
+            }`}
+          ></span>
         </div>
-        <div className={`flex ease-in-out duration-300 ${active ? "max-lg:left-[-100%]" : "max-lg:start-0"} max-lg:bg-white max-lg:flex-col max-lg:items-center max-lg:justify-center max-lg:fixed max-lg:w-full max-lg:h-full max-lg:top-0 items-center justify-center xl:gap-5 gap-4 z-20`}>
+        <div
+          className={`flex ease-in-out duration-300 ${
+            active ? "max-lg:left-[-100%]" : "max-lg:start-0"
+          } max-lg:bg-white max-lg:flex-col max-lg:items-center max-lg:justify-center max-lg:fixed max-lg:w-full max-lg:h-full max-lg:top-0 items-center justify-center xl:gap-5 gap-4 z-20`}
+        >
           {navLink.map((obj: NavLink, index: number) => (
             <Link
               key={index}
@@ -54,9 +75,16 @@ const Header = () => {
               {obj.nav_tabs}
             </Link>
           ))}
-          <div className={`flex items-center justify-center gap-3 lg:ps-3.5 ps-0 ${!active ? "max-lg:flex-col" : "flex-row"}`}>
-            <CommonButton buttonClass='!bg-white !shadow-none !border-black !border !text-black hover:!bg-blue hover:!text-white hover:!border-transparent' buttonName='Sign Up' />
-            <CommonButton buttonName='Contact Us' />
+          <div
+            className={`flex items-center justify-center gap-3 lg:ps-3.5 ps-0 ${
+              !active ? "max-lg:flex-col" : "flex-row"
+            }`}
+          >
+            <CommonButton
+              className="!bg-white !shadow-none !border-black !border !text-black hover:!bg-blue hover:!text-white hover:!border-transparent"
+              text="Sign Up"
+            />
+            <CommonButton text="Contact Us" />
           </div>
         </div>
       </div>
