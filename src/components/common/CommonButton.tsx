8856271d7a68e49cb0ba6d356390dd337ce191a1
ajay-bishnoi <AJAY2003BISHNOI@GMAIL.COM>
2024-07-25 16:55:58 +0530

    add some change in button

--- src/components/common/CommonButton.tsx
+++ src/components/common/CommonButton.tsx
@@ -9,7 +9,7 @@ const CommonButton = ({ buttonName, buttonClass, onClick }: BUTTON) => {
   return (
     <button
       onClick={onClick}
-      className={` font-normal text-lg leading-7 font-poppins rounded-full shadow-3xl text-white hover:text-black  bg-blue py-3 hover:bg-white hover:border-black duration-300 ease-linear px-6 border border-white ${buttonClass}`}
+      className={`font-normal text-lg leading-7 rounded-full shadow-3xl text-white hover:text-black  bg-blue py-3 hover:bg-white hover:border-black duration-300 ease-linear px-6 border border-white ${buttonClass}`}
     >
       {buttonName}
     </button>
