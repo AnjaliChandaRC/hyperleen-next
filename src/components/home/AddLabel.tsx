6462ac626f4085d8bd21f5490c741777bcb00e96
rinklepoonia <rinklepoonia889@gmail.com>
2024-07-26 09:11:51 +0530

    deploying

--- src/components/home/AddLabel.tsx
+++ src/components/home/AddLabel.tsx
@@ -7,7 +7,7 @@ import { PenSvg } from "../common/Icons";
 
 const AddLabel = () => {
   return (
-    <div className="relative lg:pt-20 sm:pt-10 pt-4">
+    <div id="label" className="relative lg:pt-20 sm:pt-10 pt-4">
       <div className="absolute top-[-14%] right-[12%] lg:block hidden">
         <Image
           width={398}
