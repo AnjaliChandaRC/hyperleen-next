6a43ddb73fa849c4a5a5618ccbe092073d49f9d8
Ritik <rvjaat25@gmail.com>
2024-07-26 16:15:35 +0530

    back-to-top-added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
 import Faqs from "@/components/common/Faqs";
 import GetInTouch from "@/components/common/GetInTouch";
+import BackToTopButton from "@/components/common/BackToTopButton";
 
 export const metadata: Metadata = {
   title: "Hyperleen",
@@ -29,6 +30,7 @@ export default function RootLayout({
         <Faqs />
          <GetInTouch />
         <Footer />
+        <BackToTopButton />
       </body>
     </html>
   );
