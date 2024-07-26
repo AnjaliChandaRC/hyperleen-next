fd3130cc49c617742b6f5a5a40d3467e0c8e7ab4
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-07-26 15:42:38 +0530

    manage spacing

--- src/app/rlhf/page.tsx
+++ src/app/rlhf/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
     return (
         <div>
             <Mastering />
-            <HowWorks />
+            <div className='xl:pt-[160px] lg:pt-20 pt-16'>  <HowWorks /></div>
             <EnsureHighQuality />
         </div>
     )
