aeca102cf0e77c856a206fc7e296b3c6263c3ac6
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-07-28 15:26:02 +0530

    add a section in contact-us page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,12 +1,12 @@
+import ContactUs from "@/components/ai-trainer/ContactUs";
 import ContactHero from "@/components/contact-us/ContactHero";
-import ContactUsForm from "@/components/contact-us/ContactUsForm";
 import React from "react";
 
 const page = () => {
   return (
     <>
       <ContactHero />
-      <ContactUsForm />
+      <ContactUs />
     </>
   );
 };
